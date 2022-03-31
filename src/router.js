1a22cbcf513fef931f5a9d649744f8bfb4a94317
dtumonasingh <dtumonasingh@gmail.com>
2022-04-01 00:31:37 +0530

    feat: Add Pages to routes

--- src/router.js
+++ src/router.js
@@ -1,14 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
+import Blog from '@/pages/blogs/Blog';
+import BlogList from '@/pages/blogs/BlogList';
+import Hightlight from '@/pages/highlights/Highlight';
+import HightlightList from '@/pages/highlights/HighlightsList';
+
+import NotFound from '@/pages/NotFound';
+
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/blogs', component: null },
-        { path: '/blogs/:id', component: null },
-        { path: '/hightlights', component: null },
-        { path: '/hightlights/:id', component: null },
-        { path: '/:notFound(.*)', component: null }
+        { path: '/blogs', component: BlogList },
+        { path: '/blogs/:id', component: Blog },
+        { path: '/hightlights', component: HightlightList },
+        { path: '/hightlights/:id', component: Hightlight },
+        { path: '/:notFound(.*)', component: NotFound }
     ],
 });
 
